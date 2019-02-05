c1ad5be83ad249358ad12650061ea0a5d530ebfd
mfontela <mfontela@iim.csic.es>
2019-02-05 18:05:50 +0100

    Create downloadGLODAPv2_Atlantic.R
    To download GLODAPv2 Atlantic in .mat extension from the NOAA server

---
+++ downloadGLODAPv2_Atlantic.R
@@ -0,0 +1,3 @@
+#Download the .mat file for the Atlantic in GLODAPv2
+library(utils)
+download.file("https://www.nodc.noaa.gov/archive/arc0107/0162565/2.2/data/0-data/data_product/GLODAPv2%20Atlantic%20Ocean.mat", "GLODAPv2_Atlantic.mat")
